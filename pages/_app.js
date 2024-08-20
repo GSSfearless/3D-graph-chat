6d18d54a56faf38956cf5abb14593d50a74a8245
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-08-20 22:57:46 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
-import '../styles/global.css'; // 确保引用路径无误
-import '../styles/globals.css'; // 确保引用路径无误
+import './styles/global.css'; // 确保引用路径无误
+import './styles/globals.css'; // 确保引用路径无误
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
