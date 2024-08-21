f8a045c7596537765f3dd22fd5cdc1fdbb02f9a9
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-08-21 21:40:35 +0800

    Update meme-generator.js

--- pages/api/meme-generator.js
+++ pages/api/meme-generator.js
@@ -62,12 +62,11 @@ export default async function handler(req, res) {
     // 调整文本框位置，使其更靠近 Logo
     const textPadding = 20;
     const halfLogoHeight = logoSizeHeight / 2;
-    const halfLogoWidth = logoSizeWidth / 2;
     const positions = [
       { x: canvas.width / 2, y: logoY - halfLogoHeight - textPadding - 30 }, // 上方
-      { x: logoX + logoSizeWidth + textPadding + 30, y: canvas.height / 2 }, // 右侧
+      { x: logoX + logoSizeWidth + textPadding + 40, y: canvas.height / 2 + halfLogoHeight - 20 }, // 右侧
       { x: canvas.width / 2, y: logoY + logoSizeHeight + textPadding + 30 }, // 下方
-      { x: logoX - textPadding - 30, y: canvas.height / 2 } // 左侧
+      { x: logoX - textPadding - 40, y: canvas.height / 2 + halfLogoHeight - 20 } // 左侧
     ];
 
     // 调整文本框宽度
