2aee9c6590d32bf12c4cd607af3f0f81242736f9
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-09-16 16:35:43 +0800

    Revert "test"
    This reverts commit 8bc0bc77adae6e3f88e360fd0017ce63f7286fcf.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const { i18n } = require('./next-i18next.config');
-
 module.exports = {
     env: {
       SERPER_API_KEY: process.env.SERPER_API_KEY,
       OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     },
-    i18n
   };
