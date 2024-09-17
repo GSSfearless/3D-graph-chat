b93787a5c1cb04246d1abe203bc423366092ee9a
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-09-17 16:19:00 +0800

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -441,19 +441,17 @@ export default function Search() {
         </div>
         <div className="flex justify-between items-center">
           <div className="flex items-center">
-            <Link href="/we-are-hiring">
-              <a className="block bg-[#ECF5FD] text-center p-2 rounded hover:bg-[#B6DBF7] transition duration-300 text-2xl font-medium text-gray-600">🪐</a>
-            </Link>
-            <Link href="/we-are-hiring">
-              <a className="text-xs ml-2 hover:text-blue-500 transition duration-300">We&apos;re hiring</a>
-            </Link>
+            <div className="block bg-[#ECF5FD] text-center p-2 rounded text-2xl font-medium text-gray-600">
+              🪐
+            </div>
+            <span className="text-xs ml-2 text-gray-600">We&apos;re hiring</span>
           </div>
           <div className="text-gray-400 mx-2">|</div>
           <div className="flex items-center">
-            <a href="https://discord.gg/G66pESH3gm" target="_blank" rel="noopener noreferrer" className="block bg-[#ECF5FD] text-center p-2 rounded hover:bg-[#B6DBF7] transition duration-300 text-2xl font-medium text-gray-600">
+            <div className="block bg-[#ECF5FD] text-center p-2 rounded text-2xl font-medium text-gray-600">
             🍻
-            </a>
-            <a href="https://discord.gg/G66pESH3gm" target="_blank" rel="noopener noreferrer" className="text-xs ml-2 hover:text-blue-500 transition duration-300">Join our discord</a>
+            </div>
+            <span className="text-xs ml-2 text-gray-600">Join our discord</span>
           </div>
         </div>
       </div>
