88aae9739c421befc415ff641a701e3f20ae5bc6
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-09-17 15:59:07 +0800

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -444,14 +444,16 @@ export default function Search() {
             <Link href="/about">
               <a className="block bg-[#ECF5FD] text-center p-2 rounded hover:bg-[#B6DBF7] transition duration-300 text-2xl font-medium text-gray-600">🪐</a>
             </Link>
-            <span className="text-xs ml-2">We are hiring</span>
+            <Link href="/we-are-hiring">
+              <a className="text-xs ml-2 hover:text-blue-500 transition duration-300">We are hiring</a>
+            </Link>
           </div>
           <div className="text-gray-400 mx-2">|</div>
           <div className="flex items-center">
             <a href="https://discord.gg/G66pESH3gm" target="_blank" rel="noopener noreferrer" className="block bg-[#ECF5FD] text-center p-2 rounded hover:bg-[#B6DBF7] transition duration-300 text-2xl font-medium text-gray-600">
             🍻
             </a>
-            <span className="text-xs ml-2">Join our discord</span>
+            <a href="https://discord.gg/G66pESH3gm" target="_blank" rel="noopener noreferrer" className="text-xs ml-2 hover:text-blue-500 transition duration-300">Join our discord</a>
           </div>
         </div>
       </div>
