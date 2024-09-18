d21224257189b56d7b6aebc92c7576f761ab677e
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-09-18 10:35:07 +0800

    hiring page

--- pages/we-are-hiring.tsx
+++ pages/we-are-hiring.tsx
@@ -1,3 +1,5 @@
+import '../styles/we-are-hiring.css';
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
