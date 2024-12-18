a0e5d5a13dbb14532edeb0f134bc11ab3302463a
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-12-18 16:43:25 +0800

    Revert "back"
    This reverts commit 41574368d70dce8e133e592897465f9184c273d7.

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh'],
-  },
-  defaultNS: 'common',
-  localePath: './public/locales',
-} 
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const { i18n } = require('./next-i18next.config');
-
 module.exports = {
-  i18n,
-  env: {
-    SERPER_API_KEY: process.env.SERPER_API_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
-};
+    env: {
+      SERPER_API_KEY: process.env.SERPER_API_KEY,
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    },
+  };
