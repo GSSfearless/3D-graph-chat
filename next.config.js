41574368d70dce8e133e592897465f9184c273d7
GSSfearless <44779223+GSSfearless@users.noreply.github.com>
2024-12-18 16:41:53 +0800

    back

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { i18n } = require('./next-i18next.config');
+
 module.exports = {
-    env: {
-      SERPER_API_KEY: process.env.SERPER_API_KEY,
-      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    },
-  };
+  i18n,
+  env: {
+    SERPER_API_KEY: process.env.SERPER_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
