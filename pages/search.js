550c4ea1ae5890fdcd2d5c78edea252bff3837a3
Runze Gan <e0816240@u.nus.edu>
2025-02-18 16:42:40 +0800

    debug深思未开启

--- pages/search.js
+++ pages/search.js
@@ -271,7 +271,7 @@ export default function Search() {
     } finally {
       setLoading(false);
     }
-  }, [useWebSearch]);
+  }, [useWebSearch, useDeepThinking]);
 
   useEffect(() => {
     if (q && initialLoad) {
