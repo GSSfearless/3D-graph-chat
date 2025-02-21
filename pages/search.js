b520d1baaef6f5483cb40f24b4cb4800f34e1f9f
Runze Gan <e0816240@u.nus.edu>
2025-02-21 13:44:54 +0800

    AI debug

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,5 @@
-import { faArrowRight, faDiscord } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
